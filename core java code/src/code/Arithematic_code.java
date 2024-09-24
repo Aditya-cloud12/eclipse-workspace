0bd7bd68a991beaf2bed1468dacddbbe8b821374
Aditya-cloud12 <adityaraj@Adityas-MacBook-Air.local>
2024-09-24 19:06:50 +0530

    Initial commit of Eclipse java project

---
+++ core java code/src/code/Arithematic_code.java
@@ -0,0 +1,20 @@
+package code;
+
+public class Arithematic_code {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		
+		System.out.println(c);
+		
+		
+	}
+	
+	
+	
+	
+
+}
