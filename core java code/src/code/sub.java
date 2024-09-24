77ed575aadb3b6d467e9c09625b4fd98c120c147
Aditya-cloud12 <adityaraj@Adityas-MacBook-Air.local>
2024-09-24 21:37:28 +0530

    add second code

---
+++ core java code/src/code/sub.java
@@ -0,0 +1,13 @@
+package code;
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a = 100;
+  int b = 50;
+  int c = a-b;
+  System.out.println(c);
+	}
+
+}
