adafbbd1f31434c7152316eb18a9465db2cad8cd
Aditya-cloud12 <adityaraj@Adityas-MacBook-Air.local>
2024-09-24 22:12:05 +0530

    aditya code

---
+++ core java code/src/code/Adit.java
@@ -0,0 +1,14 @@
+package code;
+
+public class Adit {
+	
+	public static void main(String[] args) {
+		
+		String a="Adtya";
+		String b =" kumar";
+		String c = a+b;
+		
+		System.out.println(c );
+	}
+
+}
